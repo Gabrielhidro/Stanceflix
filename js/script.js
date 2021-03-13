8d0c9733be4ecdcba5259d5a38a119de94ebfa6a
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-03-13 14:21:25 -0300

    add modal

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $('.owl-carousel').owlCarousel({
     nav:false,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
             items:3
@@ -14,3 +14,32 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// Modal 
+
+const modal = document.querySelector(".modal")
+const btn = document.getElementById("btn")
+const video = document.querySelector("video")
+
+btn.addEventListener('click', handleClick)
+
+function handleClick(event){
+    event.preventDefault()
+    modal.classList.add("active")
+    video.play()
+}
+
+
+function btnClose(){
+    modal.classList.remove("active")
+    video.pause()
+}
+
+// Infos
+
+const btnInfo = document.getElementById("info")
+ 
+btnInfo.addEventListener('click', moreInfo)
+function moreInfo(){
+    alert("Peugeot 206 da pau em qualquer AP!!!");
+}
